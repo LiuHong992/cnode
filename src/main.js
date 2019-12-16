e015b2bcfbfdacf8bd3a4a6a41c0b48e4d6dc7fe
自己的用户名 <992049747@qq.com>
2019-12-16 11:35:04 +0800

    cnode

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import service from './http'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.use(ElementUI);
 Vue.config.productionTip = false
+Vue.prototype.$axios = service
+    // Vue.prototype.$dayjs = dayjs
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
